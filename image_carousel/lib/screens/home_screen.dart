c534e59b1d38c51b2c3291debe30fba96c0ac664
leyng7 <leyng7@gmail.com>
2025-05-09 01:36:04 +0900

    [feat] 이미지 캐러셀 기능 추가
    - 홈 스크린에 PageView를 활용한 이미지 캐러셀 구현.
    - assets 설정 및 이미지 파일(5개) 추가.
    - 기존 예제 코드를 삭제하고 HomeScreen으로 기본 구조 변경.

---
+++ image_carousel/lib/screens/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+
+    return Scaffold(
+        body: PageView(
+          children: [1, 2, 3, 4, 5]
+            .map((number) =>
+              Image.asset('assets/image_$number.jpeg', fit: BoxFit.cover))
+            .toList(),
+      )
+    );
+  }
+}
