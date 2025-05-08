f7eab3a6cd0fe753bfbaf781aa69bcf2ea29de56
leyng7 <leyng7@gmail.com>
2025-05-08 22:08:17 +0900

    [feat] 로딩 인디케이터 추가
    - 로고 아래에 CircularProgressIndicator 추가.
    - 로딩 애니메이션 색상을 흰색으로 설정.

--- splash_screen/lib/main.dart
+++ splash_screen/lib/main.dart
@@ -12,13 +12,25 @@ class SplashScreen extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Container(
-          decoration: BoxDecoration(
-            color: Color(0xFFF99231)
-          ),
-          child: Center(
-            child: Image.asset(
-              'assets/logo.png'
-            )
+          decoration: BoxDecoration(color: Color(0xFFF99231)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/logo.png',
+                    width: 200,
+                  ),
+                  CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation(
+                      Colors.white
+                    ),
+                  )
+                ],
+              ),
+            ],
           ),
         ),
       ),
