6b918db1e5457d552ace9ccc5fc6575b1534c357
leyng7 <leyng7@gmail.com>
2025-05-09 01:49:55 +0900

    [feat] 홈 화면에 자동 슬라이드 기능 추가
    - `PageController`를 사용해 슬라이드 페이지를 제어.
    - 매 3초마다 다음 페이지로 자동 이동하도록 Timer 설정.
    - 마지막 페이지 이후 첫 페이지로 순환되는 로직 추가.

--- image_carousel/lib/screens/home_screen.dart
+++ image_carousel/lib/screens/home_screen.dart
@@ -1,16 +1,52 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
+  final PageController pageController = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer.periodic(
+      Duration(seconds: 3),
+      (timer) {
+        int? nextPage = pageController.page?.toInt();
+
+        if (nextPage == null) {
+          return;
+        }
+
+        if (nextPage == 4) {
+          nextPage = 0;
+        } else {
+          nextPage++;
+        }
+
+        pageController.animateToPage(
+          nextPage,
+          duration: Duration(microseconds: 500),
+          curve: Curves.ease
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
 
     return Scaffold(
         body: PageView(
+          controller: pageController,
           children: [1, 2, 3, 4, 5]
             .map((number) =>
               Image.asset('assets/image_$number.jpeg', fit: BoxFit.cover))
@@ -18,4 +54,5 @@ class HomeScreen extends StatelessWidget {
       )
     );
   }
+
 }
