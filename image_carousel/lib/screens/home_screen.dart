a121ad201b35cce655dbb6af3f9125041a2524b7
leyng7 <leyng7@gmail.com>
2025-05-09 09:40:46 +0900

    [feat] HomeScreen 이미지 로딩 로직 분리
    - 로컬 자산 이미지를 반환하는 메서드 `_assetImages` 추가.
    - 네트워크 이미지를 반환하는 메서드 `_networkImages` 추가.
    - 코드 가독성을 위해 이미지 로딩 부분을 함수화.

--- image_carousel/lib/screens/home_screen.dart
+++ image_carousel/lib/screens/home_screen.dart
@@ -47,12 +47,30 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
         body: PageView(
           controller: pageController,
-          children: [1, 2, 3, 4, 5]
-            .map((number) =>
-              Image.asset('assets/image_$number.jpeg', fit: BoxFit.cover))
-            .toList(),
+          children: _assetImages(),
       )
     );
   }
+
+  List<Image> _assetImages() {
+    return [1, 2, 3, 4, 5]
+        .map((number) => Image.asset('assets/image_$number.jpeg', fit: BoxFit.cover))
+        .toList();
+  }
+
+  List<Image> _networkImages() {
+    return [
+      "https://mblogthumb-phinf.pstatic.net/MjAyNDEwMDVfMjIx/MDAxNzI4MTIzOTk0NDk0.QuN4eH0oITv8ZZaKd9N2M1-XniqqKWSvdwUs_juDTAUg.WTy9-Qrivv-dodjgNesRU2tFZjagB9rrHzGje2osaW0g.JPEG/IMG_2762.JPG?type=w800",
+      "https://mblogthumb-phinf.pstatic.net/MjAyNDEwMDVfMTAy/MDAxNzI4MTIzOTk0NTE4.mPXsRq4fuijzVAdMxElP3kCzN2R7T2y4KxkYddi5rMgg.aHtbNnbjP38T0_1Qqads9IxX1sUB9fSe3j4j9B15rSMg.JPEG/IMG_2763.JPG?type=w800",
+      "https://mblogthumb-phinf.pstatic.net/MjAyNDA1MTJfMTIw/MDAxNzE1NDk4MjMzMTE5.tA-a4ODYwk27EoOQAHM4b5qtao3exAPG8R0P3xeKPMMg.LfaG2I4Ns3DEgDyi9gE5UnpOQUeZiggo6W8PA20Y1bQg.JPEG/IMG_4112.JPG?type=w800",
+      "https://mblogthumb-phinf.pstatic.net/MjAyNDA1MTJfMjk3/MDAxNzE1NDk4MjMxODQx.QRDqKDp68fm_f-U99DynI2Y3Y9kzBU37PDgsHwyZxmIg.FxQI7qhBQcJyDp03brwJINaX9-rvPSbhitToi37w0ckg.JPEG/IMG_4111.JPG?type=w800",
+      "https://mblogthumb-phinf.pstatic.net/MjAyNDA1MTJfMTA4/MDAxNzE1NDk4MjMyNTM3.Nm15XWuqOYJ6VNYuiw81M9f01sUwaziZxieEv6HD2gUg.Iv4SXFtBzMsPGpa_8WPBCchkI6zVpdhfEq6aeU8ijOog.JPEG/IMG_4109.JPG?type=w800"
+    ].map((src) => Image.network(src, fit: BoxFit.cover))
+     .toList();
+  }
+
+  // TODO 시스템에 저장된 이미지 with ImagePicker
+
+  // TODO 구글 드라이브나, 구글 포토에 있는 사진
 
 }
